972cef7cb7d0bc865ac6d47d0f11e78a37ee259c
庄伟斌 <zhuangweibin@qq.com>
2021-01-29 17:13:14 +0800

    aa

--- my-flowable/src/main/java/com/zhuang/flowable/manager/ProcessDefinitionManager.java
+++ my-flowable/src/main/java/com/zhuang/flowable/manager/ProcessDefinitionManager.java
@@ -16,6 +16,7 @@ import org.flowable.engine.impl.bpmn.behavior.ParallelMultiInstanceBehavior;
 import org.flowable.engine.impl.persistence.entity.ProcessDefinitionEntity;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.task.api.Task;
+import org.flowable.task.api.TaskInfo;
 import org.flowable.task.api.history.HistoricTaskInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -27,13 +28,13 @@ import java.util.Map;
 public class ProcessDefinitionManager {
 
     @Autowired
-    private RuntimeService runtimeService;
-    @Autowired
     private TaskService taskService;
     @Autowired
     private RepositoryService repositoryService;
     @Autowired
     private HistoryService historyService;
+    @Autowired
+    private TaskManager taskManager;
 
     public ProcessDefinitionEntity getProcessDefinitionEntityByTaskId(String taskId) {
         HistoricTaskInstance historicTaskInstance = historyService.createHistoricTaskInstanceQuery().taskId(taskId).singleResult();
@@ -52,23 +53,10 @@ public class ProcessDefinitionManager {
     }
 
     public FlowNode getFlowNodeByTaskId(String taskId) {
-        String taskDefinitionKey;
-        String processDefinitionId;
         //先从运行时获取，没有再从历史中获取
-        Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
-        if (task != null) {
-            taskDefinitionKey = task.getTaskDefinitionKey();
-            processDefinitionId = task.getProcessDefinitionId();
-        } else {
-            HistoricTaskInstance historicTaskInstance = historyService.createHistoricTaskInstanceQuery().taskId(taskId).singleResult();
-            if (historicTaskInstance == null) {
-                throw new HistoricTaskNotFoundException("taskId:" + taskId);
-            }
-            taskDefinitionKey = historicTaskInstance.getTaskDefinitionKey();
-            processDefinitionId = historicTaskInstance.getProcessDefinitionId();
-        }
-        Process process = repositoryService.getBpmnModel(processDefinitionId).getMainProcess();
-        return (FlowNode) process.getFlowElement(taskDefinitionKey);
+        TaskInfo taskInfo = taskManager.getTaskInfoByTaskId(taskId);
+        Process process = repositoryService.getBpmnModel(taskInfo.getProcessDefinitionId()).getMainProcess();
+        return (FlowNode) process.getFlowElement(taskInfo.getTaskDefinitionKey());
     }
 
     public TaskDef getTaskDefModelByFlowNode(FlowNode flowNode) {
--- my-flowable/src/main/java/com/zhuang/flowable/manager/TaskManager.java
+++ my-flowable/src/main/java/com/zhuang/flowable/manager/TaskManager.java
@@ -1,7 +1,11 @@
 package com.zhuang.flowable.manager;
 
+import com.zhuang.flowable.exception.HistoricTaskNotFoundException;
+import org.flowable.engine.HistoryService;
 import org.flowable.engine.TaskService;
 import org.flowable.task.api.Task;
+import org.flowable.task.api.TaskInfo;
+import org.flowable.task.api.history.HistoricTaskInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +14,25 @@ public class TaskManager {
 
     @Autowired
     private TaskService taskService;
+    @Autowired
+    private HistoryService historyService;
 
-    public Task getByTaskId(String taskId) {
+    public Task getTaskByTaskId(String taskId) {
         return taskService.createTaskQuery().taskId(taskId).singleResult();
+    }
+
+    public HistoricTaskInstance getHistoricTaskInstanceByTaskId(String taskId) {
+        return historyService.createHistoricTaskInstanceQuery().taskId(taskId).singleResult();
+    }
+
+    /**
+     * 先从运行时获取，没有再从历史中获取
+     * @param taskId
+     * @return
+     */
+    public TaskInfo getTaskInfoByTaskId(String taskId) {
+        Task task = getTaskByTaskId(taskId);
+        if (task != null) return task;
+        return getHistoricTaskInstanceByTaskId(taskId);
     }
 }
